1dc25813229a2adeaceede398b349499073d92eb
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2023-07-20 05:37:54 +0700

    FRONTEND - Add auth and guest middleware

---
+++ frontend/pages/guest-only.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import {definePageMeta} from "#imports";
+
+definePageMeta({
+    middleware: ['guest']
+})
+</script>
+
+<template>
+  <div>
+    Page: Guest Only
+  </div>
+</template>
+
+<style scoped></style>
